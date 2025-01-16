25ebc73a828a08ff275dfba0cdfffc29ef6ce042
bruno <bruno-p-esteves@hotmail.com>
2025-01-16 17:44:46 -0300

    refatorando testes

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,13 +1,11 @@
+import loginData from "../fixtures/login.json"
 describe('login', () => {
   beforeEach(() => {
     cy.acessarHome()
   })
   it('login realizado com sucesso', () => {
-    const usuario = {
-      email: 'teste.qa@gmail.com',
-      password: 'Teste@123!'
+    const usuario = loginData.perfilGestao
 
-    }
 
     cy.preencherFormLogin(usuario.email, usuario.password)
     cy.submitBtn('Acessar')
@@ -16,11 +14,7 @@ describe('login', () => {
   });
 
   it('não deve logar com senha inválida', () => {
-    const usuario = {
-      email: 'teste.qa@gmail.com',
-      password: 'Teste123!'
-
-    }
+    const usuario = {...loginData.perfilGestao, password: "Teste123"}
 
     cy.preencherFormLogin(usuario.email, usuario.password)
     cy.submitBtn('Acessar')
@@ -30,7 +24,7 @@ describe('login', () => {
   });
 
   it('não deve logar com dados em branco', () => {
-    
+
     cy.submitBtn('Acessar')
     //cy.verificarMsgToast('Credenciais inválidas. Verifique seu e-mail e senha.')
     cy.verificarMsgErro('O campo de e-mail é obrigatório')
